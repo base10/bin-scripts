d1e39fb39db90217c01460393be770c4864ee77b
Nathan L. Walls <nathan@wallscorp.us>
2022-12-27 22:00:51 -0500

    Parse JSON exports from Drafts and write out elements as Markdown

---
+++ drafts2markdown.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+require "debug"
+require "fileuils"
+require "json"
+
+class DraftsMarkdownConverter
+  attr_accessor :input_file, :input, :output_dir
+
+  def initialize(input_file:, output_dir:)
+    @input_file = input_file
+    @output_dir = output_dir
+  end
+
+  def run
+    read_input
+    confirm_output_dir
+    convert_drafts_json
+  end
+
+  private
+
+  def read_input
+  end
+
+  def confirm_output_dir
+  end
+
+  def convert_drafts_json
+  end
+end
+
+# TODO: Find examples of using opts parser
