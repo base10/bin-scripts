d5832ae2ba9d4b4063456119bfa42d442ace93a6
Nathan L. Walls <nathan@wallscorp.us>
2022-12-28 09:27:07 -0500

    squash! Parse JSON exports from Drafts and write out elements as Markdown

--- drafts2markdown.rb
+++ drafts2markdown.rb
@@ -24,10 +24,28 @@ class DraftsMarkdownConverter
   end
 
   def confirm_output_dir
+    # Directory should exist and be writable
+    # Future iterations might consider creating a missing directory structure
   end
 
   def convert_drafts_json
+    # iterate over the JSON and process each entry
+    # Use the metadata to create a URL safe file name leading with YYYY-MM-DD
+    # based on the creation date
+      # Disambiguate files as needed
   end
 end
 
-# TODO: Find examples of using opts parser
+# TODO: Find examples of using opts parser vs. positional args
+input_file_name = ARGV.shift
+output_dir = ARGV.shift
+
+# TODO: Check if I need Pathname
+input_file = File.path(Pathname.new(input_file_name))
+
+converter = DraftsMarkdownConverter.new(
+  input_file: input_file,
+  output_dir: output_dir
+)
+
+converter.run
