80ae0b85cda13b16286c208fb9e4195a8ff6c5da
Nathan L. Walls <nathan@wallscorp.us>
2022-12-31 16:25:17 -0500

    fixup! Parse JSON exports from Drafts and write out elements as Markdown

--- drafts2markdown.rb
+++ drafts2markdown.rb
@@ -1,16 +1,33 @@
 #!/usr/bin/env ruby
 
-# TODO: Find examples of using opts parser vs. positional args
 require "debug"
+require "optparse"
+
 require_relative "./lib/drafts_markdown_converter"
+
+options = {}
+opt_parser = OptionParser.new do |parser|
+  parser.banner = "Usage: drafts2markdown.rb [options]"
+
+  parser.on("-h", "--help", "Prints this help message") do
+    puts "Here's a help message!"
+
+    exit
+  end
+end
+
+# I'm not clear on when I need to call parse vs. parse!
+options = opt_parser.parse!
+
+p options
 
 input_file = ARGV.shift
 output_dir = ARGV.shift
 
-converter = DraftsMarkdownConverter.new(
-  file_prefix: "dayplan".freeze,
-  input_file: input_file,
-  output_dir: output_dir
-)
-
-converter.run
+# converter = DraftsMarkdownConverter.new(
+#   file_prefix: "dayplan".freeze,
+#   input_file: input_file,
+#   output_dir: output_dir
+# )
+#
+# converter.run
